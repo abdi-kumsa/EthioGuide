f26f7b077cca257773ab8364acd4cbc1a436cd8c
Abdii Kumsaa <109530418+abdi-kumsa@users.noreply.github.com>
2026-06-14 16:45:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/EthioGuide",
   images: {
     qualities: [75, 90],
+    unoptimized: true,
   },
 };
 
